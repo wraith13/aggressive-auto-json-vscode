d7efefb2203a3e9646d1c7434cf8a6b5ed85e17b
道化師 <645907+wraith13@users.noreply.github.com>
2020-11-09 19:56:00 +0900

    実装 ( WIP )

--- source/extension.ts
+++ source/extension.ts
@@ -1,21 +1,30 @@
 import * as vscode from 'vscode';
 import * as vscel from '@wraith13/vscel';
+import packageJson from "../package.json";
 const profile = vscel.profiler.profile;
+module Config
+{
+    export const root = vscel.config.makeRoot(packageJson);
+    export const enabled = root.makeEntry<boolean>("aggressiveAutoJson.enabled");
+    export const autoAddComma = root.makeEntry<boolean>("aggressiveAutoJson.autoAddComma");
+    export const autoRemoveComma = root.makeEntry<boolean>("aggressiveAutoJson.autoRemoveComma");
+    export const autoAddColon = root.makeEntry<boolean>("aggressiveAutoJson.autoAddColon");
+}
 let isMutedAll: boolean | undefined = undefined;
-class DocumentDecorationCacheEntry
+class DocumentFormatCacheEntry
 {
     isMuted: boolean | undefined;
     public constructor(document: vscode.TextDocument)
     {
-        documentDecorationCache.set(document, this);
+        documentFormatCache.set(document, this);
     }
 }
-const documentDecorationCache = new Map<vscode.TextDocument, DocumentDecorationCacheEntry>();
-const makeSureDocumentDecorationCache = (document: vscode.TextDocument) =>
-    documentDecorationCache.get(document) ?? new DocumentDecorationCacheEntry(document);
-export const updateDecoration = (textEditor: vscode.TextEditor) => profile
+const documentFormatCache = new Map<vscode.TextDocument, DocumentFormatCacheEntry>();
+const makeSureDocumentFormatCache = (document: vscode.TextDocument) =>
+    documentFormatCache.get(document) ?? new DocumentFormatCacheEntry(document);
+export const aggressiveFormat = (document: vscode.TextDocument) => profile
 (
-    "updateDecoration",
+    "aggressiveFormat",
     () =>
     {
     }
@@ -28,65 +37,57 @@ const valueThen = <ValueT, ResultT>(value: ValueT | undefined, f: (value: ValueT
     }
     return undefined;
 };
-const activeTextEditor = <T>(f: (textEditor: vscode.TextEditor) => T) => valueThen(vscode.window.activeTextEditor, f);
 const getDocumentTextLength = (document: vscode.TextDocument) => document.offsetAt
 (
     document.lineAt(document.lineCount - 1).range.end
 );
 //const isClip = (lang: string, textLength: number) => clipByVisibleRange.get(lang)(textLength / Math.max(fileSizeLimit.get(lang), 1024));
-const lastUpdateStamp = new Map<vscode.TextEditor, number>();
-export const delayUpdateDecoration = (textEditor: vscode.TextEditor): void =>
+const lastUpdateStamp = new Map<vscode.TextDocument, number>();
+export const delayAggressiveFormat = (document: vscode.TextDocument): void =>
 {
     const updateStamp = vscel.profiler.getTicks();
-    lastUpdateStamp.set(textEditor, updateStamp);
-    const textLength = getDocumentTextLength(textEditor.document);
+    lastUpdateStamp.set(document, updateStamp);
+    const textLength = getDocumentTextLength(document);
     const logUnit = 16 *1024;
     const logRate = Math.pow(Math.max(textLength, logUnit) / logUnit, 1.0 / 2.0);
-    //const lang = textEditor.document.languageId;
+    //const lang = document.languageId;
     const delay = false ? //isClip(lang, textLength) ?
             30: // clipDelay.get(lang):
             logRate *
             (
                 100 + //basicDelay.get(lang) +
                 (
-                    undefined === documentDecorationCache.get(textEditor.document) ?
+                    undefined === documentFormatCache.get(document) ?
                         100: // additionalDelay.get(lang):
                         0
                 )
             );
-    //debug(`document: ${textEditor.document.fileName}, textLength: ${textLength}, logRate: ${logRate}, delay: ${delay}`);
+    //debug(`document: ${document.fileName}, textLength: ${textLength}, logRate: ${logRate}, delay: ${delay}`);
     setTimeout
     (
         () =>
         {
-            if (lastUpdateStamp.get(textEditor) === updateStamp)
+            if (lastUpdateStamp.get(document) === updateStamp)
             {
-                lastUpdateStamp.delete(textEditor);
-                updateDecoration(textEditor);
+                lastUpdateStamp.delete(document);
+                aggressiveFormat(document);
             }
         },
         delay
     );
 };
-export const delayUpdateDecorationByDocument = (document: vscode.TextDocument): void =>
-{
-    vscode.window.visibleTextEditors
-        .filter(i => undefined !== i.viewColumn)
-        .filter(i => i.document === document)
-        .forEach(i => delayUpdateDecoration(i));
-};
 export const onDidChangeTextDocument = (document: vscode.TextDocument): void =>
 {
-    delayUpdateDecorationByDocument(document);
+    delayAggressiveFormat(document);
 };
-export const toggleMute = (textEditor: vscode.TextEditor) =>
+export const toggleMute = (document: vscode.TextDocument) =>
 {
-    const currentDocumentDecorationCache = makeSureDocumentDecorationCache(textEditor.document);
+    const currentDocumentDecorationCache = makeSureDocumentFormatCache(document);
     currentDocumentDecorationCache.isMuted =
         undefined === currentDocumentDecorationCache.isMuted ?
             ! isMutedAll:
             ! currentDocumentDecorationCache.isMuted;
-    delayUpdateDecoration(textEditor);
+    delayAggressiveFormat(document);
 };
 vscel.profiler.start();
 export const activate = (context: vscode.ExtensionContext) =>
@@ -97,14 +98,14 @@ export const activate = (context: vscode.ExtensionContext) =>
         vscode.commands.registerCommand
         (
             `aggressiveAutoJson.toggleMute`,
-            () => activeTextEditor(toggleMute)
+            () => valueThen(vscode.window.activeTextEditor?.document, toggleMute)
         ),
         vscode.commands.registerCommand
         (
             `aggressiveAutoJson.toggleMuteAll`, () =>
             {
                 isMutedAll = ! isMutedAll;
-                documentDecorationCache.forEach(i => i.isMuted = undefined);
+                documentFormatCache.forEach(i => i.isMuted = undefined);
             }
         ),
         vscode.commands.registerCommand
